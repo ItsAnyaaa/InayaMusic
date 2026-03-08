61483a7a43f7d408700d41bc5fb7362bdc2241a2
ItsAnyaaa <anyasongbot@outlook.com>
2026-03-08 08:25:48 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -32,7 +32,7 @@ from AviaxMusic.utils.formatters import check_duration, seconds_to_min, speed_co
 from AviaxMusic.utils.inline.play import stream_markup
 from AviaxMusic.utils.thumbnails import get_thumb
 from strings import get_string
-Aviax = PyTgCalls(app)
+Aviax = PyTgCalls(app).start()
 
 autoend = {}
 counter = {}
