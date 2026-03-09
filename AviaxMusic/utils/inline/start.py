4fb2cb71ba0f644cb83796b2881d9308199ddbc7
ItsAnyaaa <anyasongbot@outlook.com>
2026-03-09 14:49:00 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -1,41 +1,61 @@
-from pyrogram.types import InlineKeyboardButton
-
 import config
 from AviaxMusic import app
+from pyrogram import enums
 
 
-def start_panel(_):
+def start_panel(self, _):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            self.ikb(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+                style=enums.ButtonStyle.PRIMARY,
+                icon_custom_emoji_id=5882207227997066107,
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            self.ikb(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP,
+                icon_custom_emoji_id=5192766690609643141,
+            ),
         ],
     ]
     return buttons
 
 
-def private_panel(_):
+def private_panel(self, _):
     buttons = [
         [
-            InlineKeyboardButton(
+            self.ikb(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
+                icon_custom_emoji_id=5882207227997066107,
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            self.ikb(
+                text=_["S_B_4"],
+                callback_data="settings_back_helper",
+                icon_custom_emoji_id=5465665476971471368,
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            self.ikb(
+                text=_["S_B_5"],
+                user_id=config.OWNER_ID,
+                icon_custom_emoji_id=5433653135799228968,
+            ),
+            self.ikb(
+                text=_["S_B_2"],
+                url=config.SUPPORT_GROUP,
+                icon_custom_emoji_id=5192766690609643141,
+            ),
+        ],
+        [
+            self.ikb(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL,
+                icon_custom_emoji_id=5312012419237230426,
+            ),
         ],
     ]
     return buttons
-
-
-
-
